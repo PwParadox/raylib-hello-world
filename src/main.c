fe2df216f597f0e12df5604a1cc9b715d9a1caab
Pw <ayberk@iyigun.com>
2026-03-07 03:08:49 +0300

    Simple Raylib Hello World Program with C.

--- src/main.c
+++ src/main.c
@@ -1,39 +1,24 @@
 #include "raylib.h"
-#include "math.h"
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 800
 
-static Vector2 Vector2Scale(Vector2 v, float scale);
-static Vector2 Vector2Add(Vector2 v1, Vector2 v2);
-struct Ball {
-	Vector2 position;
-	Vector2 angle;
-	float speed;
-	int radius;
-};
-static void Bounce(struct Ball* b);
-
 int main ()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	SetTargetFPS(60);
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bouncing Ball Simulation");
-
-	struct Ball ball;
-	ball.position = (Vector2){SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
-	ball.speed = 4;
-	ball.radius = 20;
-	ball.angle = (Vector2){0.5,0.5};
-	Vector2 disp;
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World");
+	Vector2 center_pos = MeasureTextEx(GetFontDefault() , "Hello World", 20, 1);
+	center_pos = (Vector2) {SCREEN_WIDTH/2 - center_pos.x/2, SCREEN_HEIGHT/2 - center_pos.y/2};
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawCircle(ball.position.x, ball.position.y, ball.radius, RED);
+		DrawLineBezier((Vector2){0, SCREEN_HEIGHT/2}, (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT/2}, 3, GREEN);
+		DrawLineBezier((Vector2){SCREEN_WIDTH/2, 0}, (Vector2){SCREEN_WIDTH/2, SCREEN_HEIGHT}, 3, RED);
+
+		DrawText("Hello World", center_pos.x, center_pos.y, 20, WHITE);
 		EndDrawing();
-		Bounce(&ball);
 	}
 	CloseWindow();
 	return 0;
